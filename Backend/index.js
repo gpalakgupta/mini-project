b908e568a5525c0f4de4daf037125ac83e8a9b69
gpalakgupta <palakgupta5709@gmail.com>
2025-08-18 00:42:53 +0530

    initial commit

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,23 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { connectDB } from './config/db';
+
+
 const app = express();
+dotenv.config();
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
