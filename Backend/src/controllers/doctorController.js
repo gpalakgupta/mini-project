bcf54367380f96dda910b6ec648c97573f5ae191
gpalakgupta <palakgupta5709@gmail.com>
2025-08-19 11:47:15 +0530

    initial commit

--- Backend/src/controllers/doctorController.js
+++ Backend/src/controllers/doctorController.js
@@ -1,11 +1,12 @@
 import { Doctor } from '../models/Doctor.js';
 import { User } from '../models/User.js';
+import bcrypt from 'bcryptjs';
 
 // add doctor
 export const addDoctor = async (req, res) => {
     try {
-        const { name,  email, specialization, hospital, location , password } = req.body;
-        if(!email || !name || !specialization || !hospital || !location) {
+        const { name, email, specialization, hospital, location, password } = req.body;
+        if (!email || !name || !specialization || !hospital || !location) {
             return res.status(400).json({ message: 'Please fill all fields' });
         }
 
@@ -14,7 +15,7 @@ export const addDoctor = async (req, res) => {
             return res.status(400).json({ message: 'Doctor already exists' });
         }
 
-        const doctor = await Doctor.create({ name, email , specialization, hospital, location ,  password });
+        const doctor = await Doctor.create({ name, email, specialization, hospital, location, password });
         res.status(201).json(doctor);
     } catch (error) {
         console.error(error);
@@ -45,4 +46,25 @@ export const getDoctorById = async (req, res) => {
     }
 };
 
+// update doctor
+export const updateDoctor = async (req, res) => {
+    try {
+        const { name, password, specialization, hospital, location } = req.body;
+        
+        let updateFields = { name, specialization, hospital, location };
 
+        if (password) {
+            const hashpassword = await bcrypt.hash(password, 10);
+            updateFields.password = hashpassword;
+        }
+
+        const doctor = await Doctor.findByIdAndUpdate(req.params.id, updateFields, { new: true });
+        if (!doctor) {
+            return res.status(404).json({ message: 'Doctor not found' });
+        }
+        res.status(200).json(doctor);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
--- Backend/src/routes/doctorRoutes.js
+++ Backend/src/routes/doctorRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { getAllDoctors, getDoctorById,addDoctor } from "../controllers/doctorController.js";
+import { getAllDoctors, getDoctorById,addDoctor ,updateDoctor} from "../controllers/doctorController.js";
 
 const router = express.Router();
 
 router.get("/getalldoctors", getAllDoctors);
 router.get("/getdoctors/:id", getDoctorById);
 router.post("/adddoctor", addDoctor);
+router.put("/updatedoctor/:id", updateDoctor);
 
 
 export default router;
